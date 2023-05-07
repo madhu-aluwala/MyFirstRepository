fd4609304e32fea65417b1a4f0269541f8a93707
madhu-aluwala <madhu.aluwala@gmail.com>
2023-05-07 11:32:08 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 
 def main():
-    print("hello world :)")
+    #print("hello world :)")
+    print("abc")
 
 if __name__ == "__main__":
     main()
 
+print("out of main")
+print("extra ")
+
+print ('abc')
+print ('Another commit')
